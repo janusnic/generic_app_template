e77ff47f872cade4d5b0f628d308f0b569546915
Vagrant User <vagrant@vagrant-rvm>
2015-05-08 01:25:13 -0500

    Added user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions", 
+    passwords: "users/passwords", registrations: "users/registrations" }
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
