b867669e794e40f5330589ee7f9b286946465259
Vagrant User <vagrant@vagrant-rvm>
2015-05-08 18:09:59 -0500

    Added first and last names to users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 #
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(email: '007@example.com',
-      password: 'bond_james_bond', password_confirmation: 'bond_james_bond')
+    @user = User.new(last_name: 'Bond', first_name: 'James',
+      email: '007@example.com', password: 'bond_james_bond',
+      password_confirmation: 'bond_james_bond')
   end
 
   test 'should be valid' do
