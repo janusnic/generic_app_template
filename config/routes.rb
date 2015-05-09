6331d2be9a9f2792f3a85229704074be26123d59
Vagrant User <vagrant@vagrant-rvm>
2015-05-08 21:23:50 -0500

    Updated notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions',
     passwords: 'users/passwords', registrations: 'users/registrations' }
-  
-  resources :users  
-  
+
+  resources :users
+
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
