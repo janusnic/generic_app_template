b30c0d1910e3f0b2de7005a5c5ee26176e9f3cc2
Vagrant User <vagrant@vagrant-rvm>
2015-05-14 00:50:16 -0500

    Added test of valid signup at test/integration/users_signup_test.rb; updated notes

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'new'
   end
+
+  test 'valid signup information' do
+    get new_user_registration_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect user_registration_path, user: { last_name: 'Higgins',
+        first_name: 'Jonathan',
+        email: 'higgins@example.com',
+        password: 'higgy_baby',
+        password_confirmation: 'higgy_baby' }
+    end
+    assert_template 'home'
+  end
 end
