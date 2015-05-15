481938a5f9a7f7fd70818bdc60b721c78a268e7e
Vagrant User <vagrant@vagrant-rvm>
2015-05-15 16:04:10 -0500

    @user -> @u in test/integration/users_login_test.rb

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,29 +3,29 @@ require 'test_helper'
 #
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:connery) # From test/fixtures/user.yml
+    @u = users(:connery) # From test/fixtures/user.yml
   end
 
   test 'login with invalid information' do
     get new_user_session_path
-    post_via_redirect user_session_path, user: { email: @user.email,
+    post_via_redirect user_session_path, user: { email: @u.email,
       password: 'Austin Powers' }
     assert_template 'sessions/new'
     assert_not flash.empty?
     assert_select 'a[href=?]', new_user_session_path, count: 1
     assert_select 'a[href=?]', destroy_user_session_path, count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
+    assert_select 'a[href=?]', user_path(@u), count: 0
     get root_path
     assert flash.empty?
   end
 
   test 'login with valid information' do
     get new_user_session_path
-    post_via_redirect user_session_path, user: { email: @user.email,
+    post_via_redirect user_session_path, user: { email: @u.email,
       password: 'goldfinger' }
     assert_select 'a[href=?]', new_user_session_path, count: 0
     assert_select 'a[href=?]', destroy_user_session_path, count: 1
-    assert_select 'a[href=?]', user_path(@user), count: 1
+    assert_select 'a[href=?]', user_path(@u), count: 1
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -34,7 +34,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test 'logging out' do
     # Logging in
     get new_user_session_path
-    post_via_redirect user_session_path, user: { email: @user.email,
+    post_via_redirect user_session_path, user: { email: @u.email,
       password: 'goldfinger' }
     # Logging out
     delete destroy_user_session_path
@@ -42,7 +42,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_select 'a[href=?]', new_user_session_path, count: 1
     assert_select 'a[href=?]', destroy_user_session_path, count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
+    assert_select 'a[href=?]', user_path(@u), count: 0
     get root_path
     assert flash.empty?
   end
