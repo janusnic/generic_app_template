5f64cee9e7604b543baf1f038aa7a401ade426e7
Vagrant User <vagrant@vagrant-rvm>
2015-05-15 17:12:54 -0500

    Added remember_me tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -9,7 +9,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test 'login with invalid information' do
     get new_user_session_path
     post_via_redirect user_session_path, user: { email: @u.email,
-      password: 'Austin Powers' }
+      password: 'Austin Powers', remember_me: '1' }
     assert_template 'sessions/new'
     assert_not flash.empty?
     assert_select 'a[href=?]', new_user_session_path, count: 1
@@ -19,16 +19,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information, no remembering' do
     get new_user_session_path
     post_via_redirect user_session_path, user: { email: @u.email,
-      password: 'goldfinger' }
+      password: 'goldfinger', remember_me: '0' }
     assert_select 'a[href=?]', new_user_session_path, count: 0
     assert_select 'a[href=?]', destroy_user_session_path, count: 1
     assert_select 'a[href=?]', user_path(@u), count: 1
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+    assert_nil cookies['remember_user_token']
   end
 
   test 'logging out' do
@@ -45,5 +46,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', user_path(@u), count: 0
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with remembering' do
+    # Logging in
+    get new_user_session_path
+    post_via_redirect user_session_path, user: { email: @u.email,
+      password: 'goldfinger', remember_me: '1' }
+    assert_select 'a[href=?]', new_user_session_path, count: 0
+    assert_select 'a[href=?]', destroy_user_session_path, count: 1
+    assert_select 'a[href=?]', user_path(@u), count: 1
+    assert_not_nil cookies['remember_user_token']
   end
 end
