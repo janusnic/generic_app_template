bbe52bccc3d4d9008cc72c3a249c0ea08d977619
Vagrant User <vagrant@vagrant-rvm>
2015-05-30 02:46:51 -0500

    Provide login and signup tests for devise; still need to provide user edit test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,17 +2,8 @@ require 'test_helper'
 
 #
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  def login_invalid
-    @u = users(:connery) # From test/fixtures/user.yml
-    visit new_user_session_path
-    fill_in('Email', with: 'sean_connery@example.com')
-    fill_in('Password', with: 'original')
-    uncheck('Remember me')
-    click_button('Log in')
-  end
-
   def login_valid_forget
-    @u = users(:connery) # From test/fixtures/user.yml
+    @u1 = users(:connery) # From test/fixtures/user.yml
     visit new_user_session_path
     fill_in('Email', with: 'sean_connery@example.com')
     fill_in('Password', with: 'goldfinger')
@@ -21,7 +12,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   def login_remember
-    @u = users(:connery) # From test/fixtures/user.yml
+    @u1 = users(:connery) # From test/fixtures/user.yml
     visit new_user_session_path
     fill_in('Email', with: 'sean_connery@example.com')
     fill_in('Password', with: 'goldfinger')
@@ -29,49 +20,130 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     click_button('Log in')
   end
 
-  def logout
-    click_on('Logout')
-  end
-
-  test 'login with invalid information' do
-    login_invalid
+  test 'login with invalid information, no remembering' do
+    @u1 = users(:connery) # From test/fixtures/user.yml
+    visit new_user_session_path
+    fill_in('Email', with: 'sean_connery@example.com')
+    fill_in('Password', with: 'original')
+    uncheck('Remember me')
+    click_button('Log in')
     assert_equal(current_path, new_user_session_path)
     page.has_button?('Sign in')
     page.has_link? new_user_session_path
-    page.has_no_link? user_path(@u)
+    page.has_no_link? user_path(@u1)
     page.has_no_link? destroy_user_session_path
     assert_text 'Invalid email or password.'
   end
 
-  test 'login with valid information, no remembering' do
-    login_valid_forget
+  test 'login with invalid information, with remembering' do
+    @u1 = users(:connery) # From test/fixtures/user.yml
+    visit new_user_session_path
+    fill_in('Email', with: 'sean_connery@example.com')
+    fill_in('Password', with: 'original')
+    check('Remember me')
+    click_button('Log in')
+    assert_equal(current_path, new_user_session_path)
+    page.has_button?('Sign in')
+    page.has_link? new_user_session_path
+    page.has_no_link? user_path(@u1)
+    page.has_no_link? destroy_user_session_path
+    assert_text 'Invalid email or password.'
+  end
+
+  test 'login for unconfirmed user, no remembering' do
+    @u2 = users(:lazenby) # From test/fixtures/user.yml
+    visit new_user_session_path
+    fill_in('Email', with: 'george_lazenby@example.com')
+    fill_in('Password', with: 'ohmss1969')
+    uncheck('Remember me')
+    click_button('Log in')
+    assert_text 'You have to confirm your email address'
+  end
+
+  test 'login for unconfirmed user, with remembering' do
+    @u2 = users(:lazenby) # From test/fixtures/user.yml
+    visit new_user_session_path
+    fill_in('Email', with: 'george_lazenby@example.com')
+    fill_in('Password', with: 'ohmss1969')
+    check('Remember me')
+    click_button('Log in')
+    assert_text 'You have to confirm your email address'
+  end
+
+  test 'login and logout with valid information, no remembering' do
+    # Create user
+    visit new_user_registration_path
+    fill_in('Last name', with: 'Sullivan')
+    fill_in('First name', with: 'Jamie')
+    fill_in('Email', with: 'jsullivan@example.com')
+    fill_in('Password', with: 'Only Hope')
+    fill_in('Password confirmation', with: 'Only Hope')
+    click_button('Sign up')
+    @u3 = User.find_by(email: 'jsullivan@example.com')
+
+    # Confirm user
+    @u3.confirmed_at = Time.now
+    @u3.confirmation_token = nil
+    @u3.save
+
+    # Login
+    visit new_user_session_path
+    fill_in('Email', with: 'jsullivan@example.com')
+    fill_in('Password', with: 'Only Hope')
+    uncheck('Remember me')
+    click_button('Log in')
+    assert_text 'You are logged in.'
     assert_equal(current_path, root_path)
     page.has_no_button?('Sign in')
     page.has_no_link? new_user_session_path
     page.has_link? destroy_user_session_path
-    page.has_link? user_path(@u)
-    assert_text 'You are logged in.'
-    logout
-  end
+    page.has_link? user_path(@u3)
+    assert_nil @u3.remember_created_at
 
-  test 'logging out' do
-    login_valid_forget
-    logout
+    # Logout
+    click_on('Logout')
     page.has_button?('Sign in')
     page.has_link? new_user_session_path
-    page.has_no_link? user_path(@u)
+    page.has_no_link? user_path(@u3)
     page.has_no_link? destroy_user_session_path
   end
 
   # NOTE: only makes sure that the login process is successful
-  test 'login with remembering' do
-    login_remember
+  test 'login and logout with valid information, with remembering' do
+    # Create user
+    visit new_user_registration_path
+    fill_in('Last name', with: 'Carter')
+    fill_in('First name', with: 'Landon')
+    fill_in('Email', with: 'lcarter@example.com')
+    fill_in('Password', with: 'I was changed!')
+    fill_in('Password confirmation', with: 'I was changed!')
+    click_button('Sign up')
+    @u4 = User.find_by(email: 'lcarter@example.com')
+
+    # Confirm user
+    @u4.confirmed_at = Time.now
+    @u4.confirmation_token = nil
+    @u4.save
+
+    # Login
+    visit new_user_session_path
+    fill_in('Email', with: 'lcarter@example.com')
+    fill_in('Password', with: 'I was changed!')
+    check('Remember me')
+    click_button('Log in')
+    assert_text 'You are logged in.'
     assert_equal(current_path, root_path)
     page.has_no_button?('Sign in')
     page.has_no_link? new_user_session_path
     page.has_link? destroy_user_session_path
-    page.has_link? user_path(@u)
-    assert_text 'You are logged in.'
-    logout
+    page.has_link? user_path(@u4)
+    assert_nil @u4.remember_created_at
+
+    # Logout
+    click_on('Logout')
+    page.has_button?('Sign in')
+    page.has_link? new_user_session_path
+    page.has_no_link? user_path(@u4)
+    page.has_no_link? destroy_user_session_path
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,19 @@ require 'test_helper'
 
 #
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    visit new_user_registration_path
+    fill_in('Last name', with: 'Higgins')
+    fill_in('First name', with: 'Jonathan')
+    fill_in('Email', with: 'jonathan_higgins@example.com')
+    fill_in('Password', with: 'Robin Masters')
+    fill_in('Password confirmation', with: 'Zeus and Apollo')
+    click_button('Sign up')
+    assert_text 'prohibited this user from being saved'
+    u1 = User.find_by(email: 'jonathan_higgins@example.com')
+    assert_nil u1
+  end
+
   test 'valid signup information' do
     visit new_user_registration_path
     fill_in('Last name', with: 'Higgins')
@@ -10,31 +23,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     fill_in('Password', with: 'Higgy Baby')
     fill_in('Password confirmation', with: 'Higgy Baby')
     click_button('Sign up')
+    assert_text 'confirmation link has been sent to your email address'
     u1 = User.find_by(email: 'jonathan_higgins@example.com')
     assert_equal u1.last_name.to_s, 'Higgins'
     assert_equal u1.first_name.to_s, 'Jonathan'
     assert_nil u1.confirmed_at
-    assert_text 'confirmation link has been sent to your email address'
-    # url_conf = "http://localhost:3000/users/confirmation?confirmation_token=#{u1.confirmation_token}"
-    # puts url_conf
-    # visit url_conf
-    # assert_text 'Your email address has been successfully confirmed.'
-    
-    # assert_equal 1, ActionMailer::Base.deliveries.size
-    # u1 = assigns(:user)
-    # assert_not u1.activated?
-    # Try to log in before activation.
-    # log_in_as(u1)
-    # assert_not is_logged_in?
-    # Invalid activation token
-    # get edit_account_activation_path("invalid token")
-    # assert_not is_logged_in?
-    # get edit_account_activation_path(user.activation_token, email: 'wrong')
-    # assert_not is_logged_in?
-    # get edit_account_activation_path(user.activation_token, email: user.email)
-    # assert user.reload.activated?
-    # follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
   end
 end
